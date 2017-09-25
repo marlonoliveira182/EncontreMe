7e21696e2e5bb8faa26958f68cdaa1d11f5e7301
= <=>
2017-09-25 14:22:04 -0300

    implementação da camada de visualização do cadastro de desaparecido

--- PlataformaEncontreMe/Content/js/Controllers/cadastroDesaparecidoController.js
+++ PlataformaEncontreMe/Content/js/Controllers/cadastroDesaparecidoController.js
@@ -1,6 +1,65 @@
 ﻿angular.module('encontreMe').controller('cadastroDesaparecidoController', function ($scope, Map) {
 
     $scope.place = {};
+    $scope.pessoasDesaparecidas = [
+        {
+            nome: 'Lucas Fonseca',
+            dtCadastro: '25/09/2017',
+            dtUltimaAtualizacao: '25/09/2017',
+            rg: '448597845',
+            cpf: '42456985475',
+            sexo: 'M',
+            endereco: 'Endereço teste',
+            numero: '444',
+            complemento: 'casa'
+        },
+        {
+            nome: 'Caio Pires',
+            dtCadastro: '25/09/2017',
+            dtUltimaAtualizacao: '25/09/2017',
+            rg: '556894587',
+            cpf: '56936585963',
+            sexo: 'M',
+            endereco: 'Endereço teste',
+            numero: '444',
+            complemento: 'casa'
+        },
+        {
+            nome: 'Maria das Dores',
+            dtCadastro: '25/09/2017',
+            dtUltimaAtualizacao: '25/09/2017',
+            rg: '475216935',
+            cpf: '85478523696',
+            sexo: 'F',
+            endereco: 'Endereço teste',
+            numero: '444',
+            complemento: 'casa'
+        },
+        {
+            nome: 'John McAfee',
+            dtCadastro: '25/09/2017',
+            dtUltimaAtualizacao: '25/09/2017',
+            rg: '448597845',
+            cpf: '42456985475',
+            sexo: 'M',
+            endereco: 'Endereço teste',
+            numero: '444',
+            complemento: 'casa'
+        }
+    ];
+    $scope.pessoa = {};
+    $scope.isCadastro = false;
+    $scope.meuForm = '';
+
+    // Obtem acesso ao formulário passado no modal
+    $scope.InicializaFormulario = function (form) {
+        $scope.meuForm = form;
+    };
+
+    $scope.resetForm = function(){
+        $scope.meuForm.$setPristine();
+        $scope.meuForm.$setUntouched();
+    }
 
     $scope.search = function () {
         $scope.apiError = false;
@@ -25,4 +84,26 @@
 
     Map.init();
 
+    $scope.removerPessoa = function (pessoa) {
+        alert('falta implementar back end e atualizar a lista para remover a pessoa excluída')
+    }
+
+    $scope.editarPessoa = function (pessoa) {
+        $scope.pessoa = pessoa;
+        $scope.isCadastro = true;
+        $scope.resetForm();
+    }
+
+    $scope.adicionarPessoa = function () {
+        $scope.pessoa = {};
+        $scope.isCadastro = true;
+        $scope.resetForm();
+        
+    }
+
+    $scope.salvarPessoa = function () {
+        if ($scope.meuForm.$valid) {
+            alert('falta implementar back end')
+        }
+    }
 });
